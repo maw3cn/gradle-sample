03d173edbbc2c30b3e627a24de994cd4f48a275e
Michael Williams <maw3cn@virginia.edu>
2018-08-23 17:33:59 -0400

    Add assertj core

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -7,19 +7,22 @@ import java.util.Iterator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TestHelloWorld {
 
     @Test
     public void test() {
-        assertThat(1, equalTo(1));
+        assertThat(1).isEqualTo(1);
 
         Iterator<String> iter = Arrays.asList("1", "22", "3").iterator();
         assertTrue(IteratorUtils.matchesAll(iter, s -> s.length() >= 1));
 
         assertEquals("hello", "hello");
+
+        assertThat("1").isNotEqualTo("?");
 
     }
 }
